43d5a271b6463a8943ec9fd1b2f57e8b30fda7ed
mrdgdxy <xiongyuan.deng@gmail.com>
2014-12-10 17:48:19 +0100

    Push done
    Push notification works

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -40,12 +40,17 @@ var app = {
   onDeviceReady: function() {
     // Initialize push.
     push.initialize();
-
+    
     // Initialize Kinvey. Paste your app key and secret below.
     var promise = Kinvey.init({
       appKey    : 'kid_Z1jSBhbMP',
-      appSecret : 'fd0dcfe909224249bc19a013b1dcf79f'
+      appSecret : 'fd0dcfe909224249bc19a013b1dcf79f',
+      sync      : {
+          enable : true,
+          online : navigator.onLine// The initial application state.
+      }
     });
+    
     promise.then(function(activeUser) {
       // The `Kinvey.init` function returns a promise which resolves to the
       // active user data. If there is no active user, create one.
@@ -53,8 +58,73 @@ var app = {
         return Kinvey.User.signup();
       }
       return activeUser;
-    }).then(function(activeUser) {
-      // Your app is now connected to Kinvey and has an active user.
+    }).then(null, function(error) {
+    	$("#statuslabel").html("activeUsererror");
     });
+    
+    // Automated control 
+    // Switch application state when the on- and offline events fire.
+    $(window).on({
+        offline : Kinvey.Sync.offline,
+        online  : Kinvey.Sync.online
+    });
+    
+    // Add the book.
+    function addpropositions(data){
+	$("#statuslabel").html("loading");
+	var dat = { };
+ 	$.each(data, function( i, value ) {
+ 		dat["id"] = data[i].id;
+ 	});
+    
+    Kinvey.DataStore.save('propositions', dat).then(function() {
+    	$("#statuslabel").html("add propositions success");
+    }, function(error) {
+    	$("#statuslabel").html("add propositions error");
+    });
+    }
+    
+    function getpropositions(){
+    	$("#statuslabel").html("loading");
+    	var content = {};
+        Kinvey.DataStore.get('propositions', content).then(function(propositions) {
+            // Update UI.
+            content = propositions.map(function(book, index) {
+	            	$.each(content, function( i, value ) {
+ 	            		$("#datawithkinvey").append("<br>Proposition id"+i+": "+value[i].id);
+ 	            	});
+            });
+            $("#statuslabel").html("get propositions success");
+          }, function(error) {
+        	$("#statuslabel").html("get propositions error");
+          });
+    }
+
+	  $("#getdata").click(function(){
+        	//Clear the content of both 2 areas
+    		$("#datawithkinvey").empty();
+    		$("#datawithoutkinvey").empty();
+    		
+    		//Use normal ajax to load data
+ 	        $.ajax({
+ 	            dataType: "json",
+ 	            url: "https://khajour-test.apigee.net/interactionpropose_vin_dev/propose?sessiontoken=fullsix%2Ffullsix&targetId=VF1JMSE0641295422%7CFRA&maxCount=3",
+ 	            success: function(msg){
+ 	                var data = msg.ProposeResponse.propositions.propositions.proposition;
+ 	            	$.each(data, function( i, value ) {
+     	            	//Get all the proposition ids
+ 	            		$("#datawithoutkinvey").append("<br>Proposition id"+i+": "+data[i].id);
+ 	            	});
+ 	            	//Call method to add data
+ 	            	addpropositions(data);
+ 	            	getpropositions();
+ 	 			},
+              error: function (XMLHttpRequest, textStatus, errorThrown) { 
+                  alert("Please check your network connection!"); 
+             	}
+          });
+    	  });
+	  
+	  
   },
 };
